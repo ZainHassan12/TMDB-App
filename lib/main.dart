4646a02135cdcc0e843132c4e7088bc4e33b8b14
Zain <zhassan0512@gmail.com>
2024-07-06 15:08:39 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tmdb_app/homeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TMDB',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.blueGrey,
+      ),
+      home: const Homescreen(),
+    );
+  }
+}
